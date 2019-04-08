310d56a4794c08e7e8fc1ce0da74b6fb08e832fc
xbb <xbbjava@163.com>
2019-04-08 11:08:46 +0800

    mediat

---
+++ designpattern/src/designpattern/mediator/ColleagueButton.java
@@ -0,0 +1,15 @@
+package designpattern.mediator;
+
+import java.awt.*;
+
+public class ColleagueButton extends Button implements Colleague {
+    @Override
+    public void setMediator(Mediator mediator) {
+
+    }
+
+    @Override
+    public void setColleagueEnabled(boolean enabled) {
+
+    }
+}
