fae01d23be9b59690deeaf2012b8836886459134
xbb <xbbjava@163.com>
2019-04-17 10:44:11 +0800

    M

--- java/src/main/java/com/xubing/generics/Apple.java
+++ java/src/main/java/com/xubing/generics/Apple.java
@@ -2,6 +2,9 @@ package com.xubing.generics;
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author xubing xbbjava@163.com
@@ -12,8 +15,30 @@ import java.io.FileOutputStream;
 public class Apple extends Fruit {
 
     public static void main(String[] args) throws Exception{
-        Fruit apple = new Apple();
-        ((Apple) apple).fruit(apple);
+
+        /**
+         不能编译
+        List<Fruit> fruits = new ArrayList<Apple>();
+        */
+        List<? extends Fruit> fruits = new ArrayList<Apple>();
+     /*
+        不能编译通过
+        fruits.add(new Fruit());
+        fruits.add(new Apple());
+        fruits.add(new Object());*/
+        fruits.add(null);
+
+        List<? extends Apple> apples = new ArrayList<Apple>();
+       /**
+        * 编译不通过
+        *  apples.add(new Apple());
+        *  apples.add(new Object());
+        *  */
+       Fruit[] f = new Apple[10];
+       f[0] = new Apple();
+       f[1] = new Fruit(); // 可以编译通过，运行时报错
+        System.out.println(Arrays.toString(f));
+
     }
 
     public void fruit(Fruit fruit) {
