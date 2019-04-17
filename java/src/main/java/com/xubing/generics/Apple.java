f2e0de4644ef35d6d4f322680140e6809f111667
xbb <xbbjava@163.com>
2019-04-17 09:48:45 +0800

    m

--- java/src/main/java/com/xubing/generics/Apple.java
+++ java/src/main/java/com/xubing/generics/Apple.java
@@ -14,23 +14,6 @@ public class Apple extends Fruit {
     public static void main(String[] args) throws Exception{
         Fruit apple = new Apple();
         ((Apple) apple).fruit(apple);
-
-
-        FileInputStream in = new FileInputStream("D:\\workspace\\java\\JavaBasicKnowledge\\src\\main\\resources\\Vultr docker安装mysql.md");
-        FileOutputStream out = new FileOutputStream("D:\\workspace\\java\\JavaBasicKnowledge\\src\\main\\resources\\test.md");
-        byte[] buffer = new byte[20 * 1024];
-
-        // read() 最多读取 buffer.length 个字节
-        // 返回的是实际读取的个数
-        // 返回 -1 的时候表示读到 eof，即文件尾
-        while (in.read(buffer, 0, buffer.length) != -1) {
-            out.write(buffer);
-        }
-
-        in.close();
-        out.close();
-        System.out.println(1);
-
     }
 
     public void fruit(Fruit fruit) {
