f5400640cc5ac9b7654ff284467740442fd72029
xbb <xbbjava@163.com>
2019-05-13 16:48:27 +0800

    SimpleDateFormat线程不安全测试

--- java/src/main/java/com/xubing/util/Main.java
+++ java/src/main/java/com/xubing/util/Main.java
@@ -1,6 +1,12 @@
 package com.xubing.util;
 
-import jdk.nashorn.internal.runtime.Context;
+import com.beust.jcommander.internal.Lists;
+
+import java.text.SimpleDateFormat;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author xubing xbbjava@163.com
@@ -9,31 +15,27 @@ import jdk.nashorn.internal.runtime.Context;
  * @date 2018/5/28 18:55
  */
 public class Main {
-
-    private static String a = "abc";
-
     public static void main(String[] args) {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        List<String> dateStrList = Lists.newArrayList(
+                "2018-04-01 10:00:01",
+                "2018-04-02 11:00:02",
+                "2018-04-03 12:00:03",
+                "2018-04-04 13:00:04",
+                "2018-04-05 14:00:05"
+        );
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        for (String str : dateStrList) {
+            executorService.execute(() -> {
+                try {
+                    simpleDateFormat.parse(str);
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            });
+        }
 
-        int a = 5;
-        int b = 2;
-        int e = 2;
-        int c = 2;
-        int d = 2;
-        int f = 2;
-        int g = 2;
-        int h = 2;
-        int i = 12;
-        int j = 200;
 
-
-      }
-
-      public int test() {
-
-          long dd = 1;
-
-          return 1;
-
-
-       }
+    }
 }
