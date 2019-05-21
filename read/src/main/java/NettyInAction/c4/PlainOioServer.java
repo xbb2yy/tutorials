1bb0fb22e9b8e045372cc1df736ebd538c3c4c4d
xbb <xbbjava@163.com>
2019-05-21 17:54:06 +0800

    netty实战

--- read/src/main/java/NettyInAction/c4/PlainOioServer.java
+++ read/src/main/java/NettyInAction/c4/PlainOioServer.java
@@ -14,24 +14,21 @@ public class PlainOioServer {
                 final Socket clientSocket = socket.accept();
                 System.out.println(
                         "Accepted connection from " + clientSocket);
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        OutputStream out;
+                new Thread(() -> {
+                    OutputStream out;
+                    try {
+                        out = clientSocket.getOutputStream();
+                        out.write("Hi!\r\n".getBytes(
+                                Charset.forName("UTF-8")));
+                        out.flush();
+                        clientSocket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } finally {
                         try {
-                            out = clientSocket.getOutputStream();
-                            out.write("Hi!\r\n".getBytes(
-                                    Charset.forName("UTF-8")));
-                            out.flush();
                             clientSocket.close();
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        } finally {
-                            try {
-                                clientSocket.close();
-                            } catch (IOException ex) {
-                                // ignore on close
-                            }
+                        } catch (IOException ex) {
+                            // ignore on close
                         }
                     }
                 }).start();
