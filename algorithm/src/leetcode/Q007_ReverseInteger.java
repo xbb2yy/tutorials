1bd5628cca180631869a9ea5d1c671b1c03792d9
xbb <xbbjava@163.com>
2019-06-11 10:20:13 +0800

    leetcode

---
+++ algorithm/src/leetcode/Q007_ReverseInteger.java
@@ -0,0 +1,34 @@
+package leetcode;
+
+public class Q007_ReverseInteger {
+
+    public static int reverse(int x) {
+
+        boolean b = x < 0;
+        String s;
+
+        StringBuilder reverse = new StringBuilder(String.valueOf(x)).reverse();
+        if (b) {
+            s = reverse.substring(0, reverse.length() - 1);
+        } else {
+            s = reverse.toString();
+        }
+
+        try {
+            int i;
+            if (b) {
+                i = Integer.parseInt('-' + s);
+            } else {
+                i = Integer.parseInt(s);
+            }
+            return i;
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reverse(-123));
+    }
+}
