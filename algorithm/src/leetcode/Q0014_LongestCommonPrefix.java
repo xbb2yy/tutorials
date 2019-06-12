6960ac2aebd8fe41b99880d52d02bf447c6c3b30
xbb <xbbjava@163.com>
2019-06-12 14:02:10 +0800

    leetcode14

---
+++ algorithm/src/leetcode/Q0014_LongestCommonPrefix.java
@@ -0,0 +1,26 @@
+package leetcode;
+
+@Pass
+public class Q0014_LongestCommonPrefix {
+
+    public String longestCommonPrefix(String[] strs) {
+
+        if (strs.length == 0) {
+            return "";
+        }
+
+        String str = strs[0];
+
+        StringBuilder f = new StringBuilder();
+        for (int i = 0; i < str.length() ; i++) {
+            f.append(str.charAt(i));
+            for (int j = 1; j < strs.length; j++) {
+                if (!strs[j].startsWith(f.toString())) {
+                    return f.deleteCharAt(f.length() - 1 ).toString();
+                }
+            }
+        }
+
+        return f.toString();
+    }
+}
