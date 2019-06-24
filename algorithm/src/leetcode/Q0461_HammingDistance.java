b6dc68707a179fbe97a8cb059f3054a27de9ebe6
xbb <xbbjava@163.com>
2019-06-24 10:23:19 +0800

    leetcode461

---
+++ algorithm/src/leetcode/Q0461_HammingDistance.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+import java.math.BigDecimal;
+
+public class Q0461_HammingDistance {
+
+    public static int hammingDistance(int x, int y) {
+        String s = Integer.toBinaryString(x);
+        String s1 = Integer.toBinaryString(y);
+
+        for (int i = 0; i < s.length() ; i++) {
+
+        }
+
+        return -1;
+    }
+
+
+    public static void main(String[] args) {
+        hammingDistance(4, 0);
+    }
+}
