91743435919c2b0726095a9bf068496a40fdec06
xbb <xbbjava@163.com>
2019-06-24 11:04:54 +0800

    leetcode500

--- algorithm/src/leetcode/Q500_KeyboardRow.java
+++ algorithm/src/leetcode/Q500_KeyboardRow.java
@@ -1,21 +1,40 @@
 package leetcode;
 
+import leetcode.common.Pass;
+
 import java.util.*;
 
+@Pass
 public class Q500_KeyboardRow {
 
     private static Map<Character, Integer> map = new HashMap<>();
 
     static {
-        map.put('q', 1);   map.put('a', 2);   map.put('z', 3);
-        map.put('w', 1);   map.put('s', 2);   map.put('x', 3);
-        map.put('e', 1);   map.put('d', 2);   map.put('c', 3);
-        map.put('r', 1);   map.put('f', 2);   map.put('v', 3);
-        map.put('t', 1);   map.put('g', 2);   map.put('b', 3);
-        map.put('y', 1);   map.put('h', 2);   map.put('n', 3);
-        map.put('u', 1);   map.put('j', 2);   map.put('m', 3);
-        map.put('i', 1);   map.put('k', 2);
-        map.put('o', 1);   map.put('l', 2);
+        map.put('q', 1);
+        map.put('a', 2);
+        map.put('z', 3);
+        map.put('w', 1);
+        map.put('s', 2);
+        map.put('x', 3);
+        map.put('e', 1);
+        map.put('d', 2);
+        map.put('c', 3);
+        map.put('r', 1);
+        map.put('f', 2);
+        map.put('v', 3);
+        map.put('t', 1);
+        map.put('g', 2);
+        map.put('b', 3);
+        map.put('y', 1);
+        map.put('h', 2);
+        map.put('n', 3);
+        map.put('u', 1);
+        map.put('j', 2);
+        map.put('m', 3);
+        map.put('i', 1);
+        map.put('k', 2);
+        map.put('o', 1);
+        map.put('l', 2);
         map.put('p', 1);
     }
 
@@ -27,11 +46,12 @@ public class Q500_KeyboardRow {
 
         for (int i = 0; i < words.length; i++) {
             Set<Integer> s = new HashSet<>();
-            char[] chars = words[i].toCharArray();
-            for (int j = 0; j < chars.length; j++) {
-                s.add(map.get(new Character(chars[i])));
-                if (Character.isUpperCase(chars[j])) {
-                    s.add(2);
+            for (int j = 0; j < words[i].length(); j++) {
+                if (Character.isUpperCase(words[i].charAt(j))) {
+                    char c = Character.toLowerCase(words[i].charAt(j));
+                    s.add(map.get(c));
+                } else {
+                    s.add(map.get(words[i].charAt(j)));
                 }
             }
             if (s.size() == 1) {
@@ -46,7 +66,5 @@ public class Q500_KeyboardRow {
 
     public static void main(String[] args) {
 
-        String[] s = {"Alaska","Dad"};
-        findWords(s);
     }
 }
