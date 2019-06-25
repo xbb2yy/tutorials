f2092641ab658794570c6e73d6949dccbcef67b2
xbb <xbbjava@163.com>
2019-06-25 10:21:31 +0800

    leetcode709

---
+++ algorithm/src/leetcode/Q0709_ToLowerCase.java
@@ -0,0 +1,8 @@
+package leetcode;
+
+public class Q0709_ToLowerCase {
+
+    public String toLowerCase(String str) {
+       return str.toLowerCase();
+    }
+}
---
+++ algorithm/src/leetcode/Q0908_SmallestRangeI.java
@@ -0,0 +1,4 @@
+package leetcode;
+
+public class Q0908_SmallestRangeI {
+}
