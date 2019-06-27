55ac31c042d0536fd9679d7bef9290a94aa677ba
xbb <xbbjava@163.com>
2019-06-27 09:30:09 +0800

    leetcode852

---
+++ algorithm/src/leetcode/Q0852_PeakIndexinaMountainArray.java
@@ -0,0 +1,13 @@
+package leetcode;
+
+public class Q0852_PeakIndexinaMountainArray {
+
+    public int peakIndexInMountainArray(int[] A) {
+        for (int i = 1; i < A.length - 1; i++) {
+            if (A[i - 1] < A[i] && A[i] > A[i + 1]) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
