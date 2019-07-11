9e8bf6c94dc771976354dfc2edfd337392198d76
xbb <xbbjava@163.com>
2019-07-11 09:53:04 +0800

    237

---
+++ algorithm/src/leetcode/Q0237_DeleteNodeinaLinkedList.java
@@ -0,0 +1,12 @@
+package leetcode;
+
+import leetcode.common.Pass;
+
+@Pass
+public class Q0237_DeleteNodeinaLinkedList {
+
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
