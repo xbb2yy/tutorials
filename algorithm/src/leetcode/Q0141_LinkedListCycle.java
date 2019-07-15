ab93824d9de9c312b185f49eea2a2e756d0adb94
xbb <xbbjava@163.com>
2019-07-15 14:58:03 +0800

    leetcode

--- algorithm/src/leetcode/Q0141_LinkedListCycle.java
+++ algorithm/src/leetcode/Q0141_LinkedListCycle.java
@@ -8,9 +8,9 @@ import java.util.Set;
 @Pass
 public class Q0141_LinkedListCycle {
 
-    public boolean hasCycle(ListNode head) {
+    public static boolean hasCycle(ListNode head) {
 
-        ListNode current = head;
+      ListNode current = head;
 
         Set<ListNode> set = new HashSet<>();
 
@@ -23,5 +23,22 @@ public class Q0141_LinkedListCycle {
             return false;
         }
         return true;
+
+    }
+
+    public static void main(String[] args) {
+       /* ListNode node1 = new ListNode(3);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(0);
+        ListNode node4 = new ListNode(-4);
+
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node2;
+
+        hasCycle(node1);*/
+
+        System.out.println(785&234&23&25&456&23&25&456);
     }
 }
