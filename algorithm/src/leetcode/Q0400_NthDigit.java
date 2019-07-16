dee0964b32fc665212575282ba8764bb2358e029
xbb <xbbjava@163.com>
2019-07-16 10:18:41 +0800

    leetcode

---
+++ algorithm/src/leetcode/Q0400_NthDigit.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+public class Q0400_NthDigit {
+
+    public int findNthDigit(int n) {
+
+        int i = 1;
+        int m;
+        while (i < n) {
+
+        }
+
+        return -1;
+    }
+}
