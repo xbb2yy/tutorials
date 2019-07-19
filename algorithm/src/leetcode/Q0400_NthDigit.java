f3f9f661fc834715879a8386d6f8b82ac5ee9047
xbb <xbbjava@163.com>
2019-07-19 11:17:05 +0800

    leetcode

--- algorithm/src/leetcode/Q0400_NthDigit.java
+++ algorithm/src/leetcode/Q0400_NthDigit.java
@@ -1,17 +1,19 @@
 package leetcode;
 
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
 public class Q0400_NthDigit {
 
     public static int findNthDigit(int n) {
 
-        List<String> list = Stream.iterate(1, k -> k + 1).limit(n).map(a -> a + "").collect(Collectors.toList());
+        /*List<String> list = Stream.iterate(1, k -> k + 1).limit(n).map(a -> a + "").collect(Collectors.toList());
         String join = String.join("", list);
         char c = join.charAt(n);
-        return Integer.parseInt(c + "");
+        return Integer.parseInt(c + "");*/
+
+        if (n <= 9) {
+            return n;
+        }
+
+        return 0;
 
     }
 
