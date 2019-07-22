ee40a7488a286b1dc289fa34087f0df01beca735
xbb <xbbjava@163.com>
2019-07-22 09:46:36 +0800

    zha

--- algorithm/src/leetcode/Q0168_ExcelSheetColumnTitle.java
+++ algorithm/src/leetcode/Q0168_ExcelSheetColumnTitle.java
@@ -4,23 +4,29 @@ package leetcode;
 public class Q0168_ExcelSheetColumnTitle {
 
     public static String convertToTitle(int n) {
-
         StringBuilder builder = new StringBuilder();
 
         int tmp = n;
         while ((tmp > 26)) {
             int k = n % 26;
+            tmp = tmp / 26;
+            if (k == 0 && tmp <= 26) {
+                k = 26;
+                tmp--;
+            }
             if (k == 0) {
                 k = 26;
             }
             builder.insert(0, (char) (k + 64));
-            tmp = tmp / 26;
         }
-        builder.insert(0, (char) (tmp + 64));
+        if (tmp != 0) {
+
+            builder.insert(0, (char) (tmp + 64));
+        }
         return builder.toString();
     }
 
     public static void main(String[] args) {
-        System.out.println(convertToTitle(52));
+        System.out.println(convertToTitle(520));
     }
 }
