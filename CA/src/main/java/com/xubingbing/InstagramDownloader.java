b716cd207e7be29f7697b9829251bef38aad05f1
xbb <xbbjava@163.com>
2019-07-25 17:53:32 +0800

    ins

---
+++ CA/src/main/java/com/xubingbing/InstagramDownloader.java
@@ -0,0 +1,159 @@
+package com.xubingbing;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.util.List;
+
+import org.jsoup.*;
+import org.jsoup.nodes.*;
+
+public class InstagramDownloader {
+
+    private Document page;
+    private final String USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36";
+
+    /*
+    * Checks the media type and calls the appropriate method
+    * @param url The link to the instagram post
+    * @param targetDirectory The directory to store downloaded media
+    * */
+    public void downloadMedia(String url, String targetDirectory){
+        Helpers.validateURL(url);
+        try{
+                page = Jsoup.connect(url).userAgent(USER_AGENT).get();
+                String mediaType = page.select("meta[name=medium]").first()
+                        .attr("content");
+
+                switch (mediaType) {
+                    case "video":
+                        downloadVideo(url, targetDirectory);
+                        break;
+                    case "image":
+                        downloadImage(url, targetDirectory);
+                        break;
+                    default:
+                        System.out.println("Unable to download media file.");
+                        break;
+                }
+
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    /*
+    * Downloads the instagram video
+    * @param url The link to the instagram post
+    * @param targetDirectory The directory to store downloaded video
+    * */
+    public void downloadVideo(String url, String targetDirectory){
+        String videoUrl = "";
+
+        Helpers.validateURL(url);
+        try {
+                page = Jsoup.connect(url).userAgent(USER_AGENT).get();
+                videoUrl = page.select("meta[property=og:video]").first()
+                        .attr("content");
+
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+       download(videoUrl, targetDirectory);
+    }
+
+    /*
+     * Downloads the instagram image
+     * @param url The link to the instagram post
+     * @param targetDirectory The directory to store downloaded image
+     * */
+    public String downloadImage(String url, String targetDirectory){
+        String imageUrl = "";
+
+        Helpers.validateURL(url);
+        try {
+                page = Jsoup.connect(url).userAgent(USER_AGENT).get();
+                imageUrl = page.select("meta[property=og:image]").first()
+                        .attr("content");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+       return download(imageUrl, targetDirectory);
+
+
+    }
+
+    /*
+    * Fetch the download link of a media without downloading
+    * @url The link to the instagram post
+    * @return downloadUrl Returns the download link of the media found
+    * */
+    public String getDownloadUrl(String url){
+        String downloadUrl = "";
+
+        Helpers.validateURL(url);
+        try{
+                page = Jsoup.connect(url).userAgent(USER_AGENT).get();
+                String mediaType = page.select("meta[name=medium]").first()
+                        .attr("content");
+
+                switch (mediaType) {
+                    case "video":
+                        downloadUrl = page.select("meta[property=og:video]").first()
+                                .attr("content");
+                        break;
+                    case "image":
+                        downloadUrl = page.select("meta[property=og:image]").first()
+                                .attr("content");
+                        break;
+                    default:
+                        downloadUrl = "No media file found.";
+                        break;
+                }
+
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+        return downloadUrl;
+    }
+
+    /*
+     * Downloads the instagram media found
+     * @param url The link to the instagram post
+     * @param targetDirectory The directory to store downloaded media
+     * */
+    private String download(String url, String targetDirectory){
+        String[] tempName = url.split("/");
+        String filename = tempName[tempName.length-1].split("[?]")[0];
+
+        try(InputStream inputStream = URI.create(url).toURL().openStream()){
+            HttpURLConnection conn = (HttpURLConnection)URI.create(url).toURL().openConnection();
+
+            Path targetPath = new File(targetDirectory + File.separator + filename).toPath();
+            Files.copy(inputStream, targetPath, StandardCopyOption.REPLACE_EXISTING);
+
+            int BYTES_PER_KB = 1024;
+            double fileSize = ((double)conn.getContentLength() / BYTES_PER_KB);
+
+            System.out.println("Media file downloaded successfully.");
+            System.out.println(String.format("Media Location: %s", targetPath));
+            System.out.println(String.format("Media Name: %s", filename));
+            System.out.println(String.format("Media Size: %.2f kb", fileSize));
+            System.out.println(String.format("Media Type: %s", Helpers.mediaType(filename)));
+            return targetPath.toString();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+}
--- CA/src/main/java/com/xubingbing/Main.java
+++ CA/src/main/java/com/xubingbing/Main.java
@@ -1,10 +1,10 @@
 package com.xubingbing;
 
+import org.apache.http.HttpHost;
 import org.brunocvcunha.instagram4j.Instagram4j;
-import org.brunocvcunha.instagram4j.requests.InstagramGetMediaInfoRequest;
-import org.brunocvcunha.instagram4j.requests.InstagramGetUserReelMediaFeedRequest;
-import org.brunocvcunha.instagram4j.requests.InstagramUploadAlbumRequest;
-import org.brunocvcunha.instagram4j.requests.InstagramUploadPhotoRequest;
+import org.brunocvcunha.instagram4j.requests.*;
+import org.brunocvcunha.instagram4j.requests.payload.*;
+import org.jsoup.nodes.Document;
 
 import java.io.File;
 import java.util.UUID;
@@ -12,10 +12,25 @@ import java.util.UUID;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        Instagram4j instagram = Instagram4j.builder().username("dogogdaily").password("A94vTmnYGfFEN8y").build();
 
-        instagram.setup();
-        instagram.login();
+
+        String proxyHost = "127.0.0.1";
+        String proxyPort = "1080";
+        System.setProperty("http.proxyHost", proxyHost);
+        System.setProperty("http.proxyPort", proxyPort);
+// 对https也开启代理
+        System.setProperty("https.proxyHost", proxyHost);
+        System.setProperty("https.proxyPort", proxyPort);
+        String instaImage = "https://www.instagram.com/p/B0LRw_zDsVo/";
+        String instaVideo = "https://www.instagram.com/p/BtT2UomAH9l/";
+        String directory = "./";
+
+        InstagramDownloader instagram = new InstagramDownloader();
+
+        String s = instagram.downloadImage(instaImage, directory);
+        File file = new File(s);
+        System.out.println(file.exists());
+        System.out.println(s);
 
     }
 
