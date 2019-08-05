22adf147f9b23ef71dffcd62e7eca42a7310c8ea
xbb <xbbjava@163.com>
2019-08-05 14:54:01 +0800

    algs

---
+++ algorithm/src/algorithm/c1/DoublingTest.java
@@ -0,0 +1,26 @@
+package algorithm.c1;
+
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.Stopwatch;
+
+public class DoublingTest {
+
+    public static double timeTrial(int N) {
+        int max = 1000000;
+        int[] a = new int[N];
+        for (int i = 0; i < N; i++) {
+            a[i] = StdRandom.uniform(-max, max);
+        }
+        Stopwatch timer = new Stopwatch();
+        int cnt = ThreeSum.count(a);
+        return timer.elapsedTime();
+    }
+
+    public static void main(String[] args) {
+        for (int i = 250; true; i += i) {
+            double time = timeTrial(i);
+            StdOut.printf("%-7d%5.1f\n", i, time);
+        }
+    }
+}
