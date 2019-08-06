cc8c9530400acac4b7df7ca7bc60cf4cb5a51ff8
xbb <xbbjava@163.com>
2019-08-06 14:35:20 +0800

    q0013

--- algorithm/src/leetcode/Q0013_RomantoInteger.java
+++ algorithm/src/leetcode/Q0013_RomantoInteger.java
@@ -9,29 +9,20 @@ import java.util.Map;
 public class Q0013_RomantoInteger {
 
     public int romanToInt(String s) {
-
         int sum = 0;
         Map<Character, Integer> map = new HashMap<>();
-        map.put('I',   1);
-        map.put('V',   5);
-        map.put('X',  10);
-        map.put('L',  50);
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
         map.put('C', 100);
         map.put('D', 500);
-        map.put('M',1000);
+        map.put('M', 1000);
         char[] chars = s.toCharArray();
 
         for (int i = 0; i < chars.length; i++) {
-            if (chars[i] == 'I' || chars[i] == 'X' || chars[i] == 'C' ) {
-                if (i + 1 < chars.length) {
-                    if (map.get(chars[i]) < map.get(chars[i + 1])) {
-                        sum -= map.get(chars[i]);
-                    } else {
-                         sum += map.get(chars[i]);
-                    }
-                } else {
-                    sum += map.get(chars[i]);
-                }
+            if (i + 1 < chars.length && map.get(chars[i]) < map.get(chars[i + 1])) {
+                sum -= map.get(chars[i]);
             } else {
                 sum += map.get(chars[i]);
             }
