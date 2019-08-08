9ec2c6dd7ac65eb3d3d44f88f1e973ff42f2efbf
xbb <xbbjava@163.com>
2019-08-08 16:02:54 +0800

    two sum O(n)解法

--- algorithm/src/leetcode/Q0001_TwoSum.java
+++ algorithm/src/leetcode/Q0001_TwoSum.java
@@ -2,6 +2,9 @@ package leetcode;
 
 import leetcode.common.Pass;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Pass
 public class Q0001_TwoSum {
 
@@ -17,6 +20,28 @@ public class Q0001_TwoSum {
             }
         }
 
+        return a;
+    }
+
+    /**
+     * O(n) 解法
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] solution(int[] nums, int target) {
+
+        int[] a = new int[2];
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length ; i++) {
+            if (map.containsKey(target - nums[i])) {
+                a[0] = map.get(target - nums[i]);
+                a[1] = i;
+                break;
+            } else {
+                map.put(nums[i], i);
+            }
+        }
         return a;
     }
 }
