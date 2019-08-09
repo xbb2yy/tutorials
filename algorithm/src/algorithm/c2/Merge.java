aa39cc511f54e55dc7f8b79bf1b05e1283bfb45e
xbb <xbbjava@163.com>
2019-08-09 16:51:29 +0800

    贵归并排序

--- algorithm/src/algorithm/c2/Merge.java
+++ algorithm/src/algorithm/c2/Merge.java
@@ -1,4 +1,47 @@
 package algorithm.c2;
 
+import javax.sound.midi.Soundbank;
+
+import static algorithm.c2.Example.isSorted;
+import static algorithm.c2.Example.show;
+
 public class Merge {
+
+    private static Comparable[] aux;
+
+    public static void sort(Comparable[] a) {
+        aux = new Comparable[a.length];
+        sort(a, 0, a.length - 1);
+    }
+
+    private static void sort(Comparable[] a, int lo, int hi) {
+        if (hi <= lo) return;
+        int mid = lo + (hi - lo) / 2;
+        sort(a, lo, mid);
+        sort(a, mid + 1, hi);
+        merge(a, lo, mid, hi);
+    }
+
+    public static void merge(Comparable[] a, int lo, int mid, int hi) {
+        int i = lo; int j = mid + 1;
+        for (int k = lo; k <= hi; k++) {
+            aux[k] = a[k];
+        }
+
+        for (int k = lo; k <= hi; k++) {
+            if (i > mid) a[k] = aux[j++];
+            else if (j > hi) a[k] = aux[i++];
+            else if(Example.less(aux[j], aux[i])) a[k] = aux[j++];
+            else a[k] = aux[i++];
+        }
+    }
+
+    public static void main(String[] args) {
+        Integer[] a = {32, 78, 22, 34, 43, 2, 66, 23, 77, 55};
+        sort(a);
+        assert isSorted(a);
+        show(a);
+    }
+
+
 }
---
+++ algorithm/src/leetcode/Q0005_LongestPalindromicSubstring.java
@@ -0,0 +1,10 @@
+package leetcode;
+
+/**
+ * 找出字符串中最大的回文串
+ */
+public class Q0005_LongestPalindromicSubstring {
+    public String longestPalindrome(String s) {
+        return null;
+    }
+}
