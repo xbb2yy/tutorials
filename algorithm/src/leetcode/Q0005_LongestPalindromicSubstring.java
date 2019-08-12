df8fce02d3742c667d77b0bf57ee20ca44c519b2
xbb <xbbjava@163.com>
2019-08-12 11:24:57 +0800

    最大回文串

--- algorithm/src/leetcode/Q0005_LongestPalindromicSubstring.java
+++ algorithm/src/leetcode/Q0005_LongestPalindromicSubstring.java
@@ -4,8 +4,47 @@ package leetcode;
  * 找出字符串中最大的回文串
  */
 public class Q0005_LongestPalindromicSubstring {
-    public String longestPalindrome(String s) {
+    public static String longestPalindrome(String s) {
 
-        return null;
+        int N = s.length();
+        int start = 0;
+        int end = 0;
+        int max = 1;
+        for (int i = 0; i < N; i++) {
+            int k = 1;
+            int l = 0;
+            for (int j = 1; i - j >= 0 && i + j < N; j++) {
+                if (s.charAt(i - j) == s.charAt(i + j)) {
+                    k += 2;
+                } else {
+                    break;
+                }
+            }
+
+            for (int j = 1; i - j >= 0 && i + j < N; j++) {
+                if (s.charAt(i - j + 1) == s.charAt(i + j)) {
+                    l += 2;
+                } else {
+                    break;
+                }
+            }
+
+            if (k > l && k > max) {
+                start = i - (l) / 2 - 1;
+                end = i + (l) / 2 + 1;
+            }
+            if (l > k && l > max) {
+                start = i - l / 2 + 2;
+                end = i + l  / 2;
+            }
+        }
+        if (start != 0 || end != 0) {
+            return s.substring(start, end + 1);
+        }
+        return s.substring(0, 1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(longestPalindrome("abba"));
     }
 }
