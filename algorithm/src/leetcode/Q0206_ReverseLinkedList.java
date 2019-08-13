690b2bd32de74d876aad712543ad81f204b6ad6f
xbb <xbbjava@163.com>
2019-08-13 13:54:28 +0800

    链表的翻转

--- algorithm/src/leetcode/Q0206_ReverseLinkedList.java
+++ algorithm/src/leetcode/Q0206_ReverseLinkedList.java
@@ -1,23 +1,39 @@
 package leetcode;
 
+/**
+ * 链表的翻转
+ */
 public class Q0206_ReverseLinkedList {
 
-    public ListNode reverseList(ListNode head) {
+    public static ListNode reverseList(ListNode head) {
 
         if (head == null || head.next == null) {
             return head;
         }
-        ListNode h = head; ListNode n = null;
-        while (h != null) {
-            ListNode tmp = h.next;
-            h.next = n;
-            n = h;
-            h = tmp;
+       ListNode n = null; ListNode cur = head;
+        while (cur != null) {
+           ListNode tmp = cur.next;
+           cur.next = n;
+           n = cur;
+           cur = tmp;
         }
         return n;
     }
 
     public static void main(String[] args) {
+        ListNode l1 = new ListNode(1);
+        ListNode l2 = new ListNode(2);
+        ListNode l3 = new ListNode(3);
+        ListNode l4 = new ListNode(4);
+        ListNode l5 = new ListNode(5);
 
-    }
+        l1.next = l2;
+        l2.next = l3;
+        l3.next = l4;
+        l4.next = l5;
+        reverseList(l1);
+
+     }
 }
+
+
