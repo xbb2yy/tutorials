b6e69ab1e077230cc5b164c09b1c9dc55861bfbc
xbb <xbbjava@163.com>
2019-08-20 10:46:39 +0800

    371

--- algorithm/src/leetcode/Q0371_SumofTwoIntegers.java
+++ algorithm/src/leetcode/Q0371_SumofTwoIntegers.java
@@ -3,7 +3,15 @@ package leetcode;
 public class Q0371_SumofTwoIntegers {
 
     public int getSum(int a, int b) {
-        // TODO
-        return -1;
+        while (b != 0) {
+            int c = a & b;
+            a = a ^ b;
+            b = c << 1;
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(1 & 1);
     }
 }
