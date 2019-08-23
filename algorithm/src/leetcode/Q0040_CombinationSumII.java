bba93a6dbc054cd1ca29b163dd063ddf17fa960f
xbb <xbbjava@163.com>
2019-08-23 14:32:57 +0800

    Q0040_CombinationSumII

--- algorithm/src/leetcode/Q0040_CombinationSumII.java
+++ algorithm/src/leetcode/Q0040_CombinationSumII.java
@@ -1,9 +1,12 @@
 package leetcode;
 
+import leetcode.common.Pass;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+@Pass
 public class Q0040_CombinationSumII {
 
     public static List<List<Integer>> combinationSum2(int[] candidates, int target) {
@@ -14,32 +17,26 @@ public class Q0040_CombinationSumII {
         return list;
     }
 
-    // TODO  重复元素
     public static void find(int[] a, int t, List<List<Integer>> list, List<Integer> l, int n) {
-        if (n >= a.length || t < a[n]) {
+        if (t == 0) {
+            list.add(new ArrayList<>(l));
+        }
+        if (t < 0) {
             return;
         }
-
-        int i = Arrays.binarySearch(a, n, a.length, t);
-        if (i >= 0) {
-            List<Integer> m = new ArrayList<>();
-            m.addAll(l);
-            m.add(a[i]);
-            list.add(m);
-        }
-        for (int j = n; j < a.length; j++) {
-            if (j == i) {
+        for (int i = n; i < a.length; i++) {
+            // 可以去重复
+            if (i > n && a[i] == a[i - 1]) {
                 continue;
             }
-            List<Integer> k = new ArrayList<>();
-            k.addAll(l);
-            k.add(a[j]);
-            find(a, t - a[j], list, k, j + 1);
+            List<Integer> m = new ArrayList<>(l);
+            m.add(a[i]);
+            find(a, t - a[i], list, m, i + 1);
         }
     }
 
     public static void main(String[] args) {
-        int[] a = {10,1,2,7,6,1,5};
+        int[] a = {10, 1, 2, 7, 6, 1, 5};
         List<List<Integer>> lists = combinationSum2(a, 8);
         System.out.println(lists);
     }
