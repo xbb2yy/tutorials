b1c707a4e14d61113a75d51837d9738d9cbb64bb
xbb <xbbjava@163.com>
2019-08-23 15:12:01 +0800

    leetcodeTeemoAttacking

---
+++ algorithm/src/leetcode/Q0_495_TeemoAttacking.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+import leetcode.common.Pass;
+
+@Pass
+public class Q0_495_TeemoAttacking {
+
+    public int findPoisonedDuration(int[] timeSeries, int duration) {
+
+        int N = timeSeries.length;
+        if (N == 0) {
+            return 0;
+        }
+        int sum = 0;
+        for (int i = 1; i < N; i++) {
+            if (timeSeries[i] - timeSeries[i - 1] > duration) {
+                sum += duration;
+            } else {
+                sum += (timeSeries[i] - timeSeries[i - 1]);
+            }
+        }
+        return (sum += duration);
+    }
+}
