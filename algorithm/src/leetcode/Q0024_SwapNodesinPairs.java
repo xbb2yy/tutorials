1e2560faee2dd99a4b3cb1174845f5431ab3068c
xbb <xbbjava@163.com>
2019-08-23 10:30:06 +0800

    交换链表中相邻的两个元素

--- algorithm/src/leetcode/Q0024_SwapNodesinPairs.java
+++ algorithm/src/leetcode/Q0024_SwapNodesinPairs.java
@@ -8,7 +8,9 @@ public class Q0024_SwapNodesinPairs {
         ListNode prev = null;
         while (l != null) {
             if (l.next == null) {
-                prev.next = l;
+                if (prev != null) {
+                    prev.next = l;
+                }
                 return h;
             }
             ListNode tmp = l.next.next;
@@ -16,9 +18,11 @@ public class Q0024_SwapNodesinPairs {
                 h = l.next;
                 prev = l;
                 l.next.next = l;
+                l.next = null;
             } else {
                 prev.next = l.next;
                 l.next.next = l;
+                l.next = null;
                 prev = l;
             }
             l = tmp;
@@ -30,16 +34,9 @@ public class Q0024_SwapNodesinPairs {
     public static void main(String[] args) {
         ListNode l1 = new ListNode(1);
         ListNode l2 = new ListNode(2);
-        ListNode l3 = new ListNode(3);
-        ListNode l4 = new ListNode(4);
-        ListNode l5 = new ListNode(5);
-        ListNode l6 = new ListNode(6);
 
         l1.next = l2;
-        l2.next = l3;
-        l3.next = l4;
-        l4.next = l5;
-        l5.next = l6;
+
 
         swapPairs(l1);
 
