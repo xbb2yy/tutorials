560d2aca795f7e45fd4f58bdf4c74c1e1cf765b0
xbb <xbbjava@163.com>
2019-08-23 15:02:17 +0800

    能否中下指定数量的花

--- algorithm/src/leetcode/Q0605_CanPlaceFlowers.java
+++ algorithm/src/leetcode/Q0605_CanPlaceFlowers.java
@@ -1,23 +1,35 @@
 package leetcode;
 
+import leetcode.common.Pass;
+
+@Pass
 public class Q0605_CanPlaceFlowers {
 
+    public static boolean canPlaceFlowers(int[] flowerbed, int n) {
 
-    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        boolean prev = false;
+        int sum = 0;
+        for (int i = 0; i < flowerbed.length;) {
+            if (prev) {
+                prev = flowerbed[i] == 1;
+                i++;
+            } else {
+                if (flowerbed[i] == 0 && ((i + 1 < flowerbed.length && flowerbed[i + 1] == 0) || i == flowerbed.length - 1) ) {
+                    sum++;
+                    prev = false;
+                    i += 2;
+                } else {
+                    prev = flowerbed[i] == 1;
+                    i++;
+                }
 
-        int m = 0;
-        for (int i = 0; i < flowerbed.length ;) {
-
-            if (flowerbed[i] == 1) {
-                i += 2;
-            }
-            if (flowerbed[i] == 0 &&  flowerbed[i - 1] == 0  && flowerbed[i + 1] == 0 ) {
-                m++;
-                i += 2;
             }
         }
+        return n <= sum;
+    }
 
-        return false;
-
+    public static void main(String[] args) {
+        int[] a = {1,0,0,0,1,0,0};
+        canPlaceFlowers(a, 2);
     }
 }
