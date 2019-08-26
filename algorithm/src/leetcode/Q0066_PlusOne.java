caddee87b1a56321c7d7d544af8ba15b22771bd0
xbb <xbbjava@163.com>
2019-08-26 09:41:31 +0800

    plusone

--- algorithm/src/leetcode/Q0066_PlusOne.java
+++ algorithm/src/leetcode/Q0066_PlusOne.java
@@ -3,8 +3,8 @@ package leetcode;
 import leetcode.common.Pass;
 
 @Pass
-public class Q0066_PlusOne {
-    public int[] plusOne(int[] digits) {
+public  class Q0066_PlusOne {
+    public static int[] plusOne(int[] digits) {
         digits[digits.length-1]++;
         int carry = 0;
         for(int i=digits.length-1;i>=0;i--){
@@ -21,5 +21,10 @@ public class Q0066_PlusOne {
             newDigits[i+1] = digits[i];
         }
         return newDigits;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {1, 2, 3};
+        plusOne(a);
     }
 }
