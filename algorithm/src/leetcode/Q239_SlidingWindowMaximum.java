4253721f55c5c3dfd7e86457a5b0dd97c93d2db3
xbb <xbbjava@163.com>
2019-08-28 11:19:30 +0800

    SlidingWindowMaximum

---
+++ algorithm/src/leetcode/Q239_SlidingWindowMaximum.java
@@ -0,0 +1,31 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class Q239_SlidingWindowMaximum {
+
+
+    public static int[] maxSlidingWindow(int[] nums, int k) {
+
+        if (nums.length == 0) {
+            return new int[0];
+        }
+        int[] n = new int[nums.length - k + 1];
+
+        for (int i = 0; i <= nums.length - k; i++) {
+            int max = nums[i];
+            for (int j = i; j < k + i; j++) {
+                if (nums[j] > max) {
+                    max = nums[j];
+                }
+            }
+            n[i] = max;
+        }
+        return n;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {1, -1};
+        System.out.println(Arrays.toString(maxSlidingWindow(a, 3)));
+    }
+}
