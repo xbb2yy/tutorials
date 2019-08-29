32b1382ae5f3982bed3ed545c31037b90fe06e8a
xbb <xbbjava@163.com>
2019-08-29 09:46:48 +0800

    ContainerWithMostWater

---
+++ algorithm/src/leetcode/Q0011_ContainerWithMostWater.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+import leetcode.common.Pass;
+
+@Pass
+public class Q0011_ContainerWithMostWater {
+
+    public int maxArea(int[] height) {
+
+        int max = 0;
+        for (int i = 0; i < height.length; i++) {
+            for (int j = i + 1; j < height.length; j++) {
+               max = Math.max(max,  Math.min(height[i], height[j]) * (j - i));
+            }
+        }
+        return max;
+    }
+
+
+}
