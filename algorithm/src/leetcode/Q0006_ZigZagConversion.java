5e9313a402e0f823d62631104c37e5d29f911f96
xbb <xbbjava@163.com>
2019-08-29 16:23:50 +0800

    ZigZagConversion

--- algorithm/src/leetcode/Q0006_ZigZagConversion.java
+++ algorithm/src/leetcode/Q0006_ZigZagConversion.java
@@ -2,9 +2,39 @@ package leetcode;
 
 public class Q0006_ZigZagConversion {
 
-    public String convert(String s, int numRows) {
+    public static String convert(String s, int numRows) {
 
+        /**
+         * 第一次提交没加这个判断导致死循环，java heap溢出
+         */
+        if (numRows == 1) {
+            return s;
+        }
+        StringBuilder r = new StringBuilder();
+        for (int i = 0; i < numRows; i++) {
+            boolean flag = true;
+            StringBuilder sb = new StringBuilder();
+            for (int j = i; j < s.length(); ) {
+                sb.append(s.charAt(j));
+                if (i == 0 || i == numRows - 1) {
+                    j += (numRows - 1) * 2;
+                } else {
+                    if (flag) {
+                        j += (numRows * 2 - 2) - i * 2;
+                        flag = false;
+                    } else {
+                        j += 2 * i;
+                        flag = true;
+                    }
+                }
 
-        return null;
+            }
+            r.append(sb);
+        }
+        return r.toString();
+    }
+
+    public static void main(String[] args) {
+        convert("A",1);
     }
 }
