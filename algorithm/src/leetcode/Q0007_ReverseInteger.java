8d65ea0caa7719c2f4f84dbc0811ce79041269e7
xbb <xbbjava@163.com>
2019-08-29 14:03:50 +0800

    int翻转

--- algorithm/src/leetcode/Q0007_ReverseInteger.java
+++ algorithm/src/leetcode/Q0007_ReverseInteger.java
@@ -32,6 +32,20 @@ public class Q0007_ReverseInteger {
         return 0;
     }
 
+    public static int solution(int x) {
+        int ans = 0;
+        while (x != 0) {
+            int pop = x % 10;
+            if (ans > Integer.MAX_VALUE / 10 || (ans == Integer.MAX_VALUE / 10 && pop > 7))
+                return 0;
+            if (ans < Integer.MIN_VALUE / 10 || (ans == Integer.MIN_VALUE / 10 && pop < -8))
+                return 0;
+            ans = ans * 10 + pop;
+            x /= 10;
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
         System.out.println(reverse(-123));
     }
