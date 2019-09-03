3095dcf17562beb76ad72cfea67c3e2f5df3a57a
xbb <xbbjava@163.com>
2019-09-03 15:32:55 +0800

    leetcode

---
+++ algorithm/src/leetcode/Q1178_NumberofValidWordsforEachPuzzle.java
@@ -0,0 +1,12 @@
+package leetcode;
+
+import java.util.List;
+
+public class Q1178_NumberofValidWordsforEachPuzzle {
+
+    public List<Integer> findNumOfValidWords(String[] words, String[] puzzles) {
+        int[] a = new int[puzzles.length];
+
+        return null;
+    }
+}
