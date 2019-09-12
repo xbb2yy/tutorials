3611e7d8fa0645a87abd6e85d555387ed2738f38
xbb <xbbjava@163.com>
2019-09-12 10:12:04 +0800

    NaryTreePostorderTraversal

---
+++ algorithm/src/leetcode/Q_0145_BinaryTreePostorderTraversal.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+import leetcode.common.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Q_0145_BinaryTreePostorderTraversal {
+
+    private List<Integer> list = new ArrayList<>();
+    public List<Integer> postorderTraversal(TreeNode root) {
+        if (root == null) {
+            return list;
+        }
+        postorderTraversal(root.left);
+        postorderTraversal(root.right);
+        list.add(root.val);
+        return list;
+    }
+}
---
+++ algorithm/src/leetcode/Q_590_NaryTreePostorderTraversal.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+import leetcode.common.Node;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Q_590_NaryTreePostorderTraversal {
+
+    List<Integer> list = new ArrayList<>();
+    public List<Integer> postorder(Node root) {
+        if (root == null) {
+            return list;
+        }
+        List<Node> children = root.children;
+        for (int i = 0; i < children.size(); i++) {
+            postorder(children.get(i));
+        }
+        list.add(root.val);
+        return list;
+    }
+}
