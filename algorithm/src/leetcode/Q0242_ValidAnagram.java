06029b91d8b3d78355aab6c5856ea26bade591cd
xbb <xbbjava@163.com>
2019-09-13 16:25:48 +0800

    ValidAnagram

---
+++ algorithm/src/leetcode/Q0242_ValidAnagram.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class Q0242_ValidAnagram {
+
+    public boolean isAnagram(String s, String t) {
+        char[] s1 = s.toCharArray();
+        char[] t1 = t.toCharArray();
+        Arrays.sort(s1);
+        Arrays.sort(t1);
+        return Arrays.equals(s1, t1);
+    }
+}
