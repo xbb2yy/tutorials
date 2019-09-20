44c14a97f8112a01c5ad6cce3b27337b5be6b19e
xbb <xbbjava@163.com>
2019-09-20 14:33:58 +0800

    ugly number no recursive

--- algorithm/src/leetcode/Q0263_UglyNumber.java
+++ algorithm/src/leetcode/Q0263_UglyNumber.java
@@ -20,4 +20,29 @@ public class Q0263_UglyNumber {
         }
         return false;
     }
+
+    public boolean solution(int num) {
+        if (num <= 0) {
+            return false;
+        }
+        int n = num;
+        while (true) {
+            int t = n;
+            if (num == 1) {
+                return true;
+            }
+            if (num % 2 == 0) {
+                n = n / 2;
+            }
+            if (num % 3 == 0) {
+                n = n / 3;
+            }
+            if (num % 5 == 0) {
+                n = n / 5;
+            }
+            if (t == n) {
+                return false;
+            }
+        }
+    }
 }
