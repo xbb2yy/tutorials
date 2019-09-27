9b1f53f06dec9c3892d29dae7040325441a0582a
xbb <xbbjava@163.com>
2019-09-27 14:27:57 +0800

    303

--- algorithm/src/leetcode/Q0303_RangeSumQuery_Immutable.java
+++ algorithm/src/leetcode/Q0303_RangeSumQuery_Immutable.java
@@ -1,8 +1,12 @@
 package leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Q0303_RangeSumQuery_Immutable {
 
     private int[] a;
+    Map<String, Integer> map = new HashMap<>();
 
     public Q0303_RangeSumQuery_Immutable(int[] nums) {
         a = new int[nums.length];
@@ -12,9 +16,13 @@ public class Q0303_RangeSumQuery_Immutable {
     public int sumRange(int i, int j) {
 
         int sum = 0;
+        if (map.containsKey(i + ":" + j)) {
+            return map.get(i + ":" + j);
+        }
         for (int k = i; k <= j; k++) {
             sum += a[k];
         }
+        map.put(i + ":" + j, sum);
         return sum;
     }
 }
