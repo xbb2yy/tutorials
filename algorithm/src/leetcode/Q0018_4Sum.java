20ee27fa1e33a4f1222a38f15fb5bc368e678041
xbb <xbbjava@163.com>
2019-10-09 17:55:08 +0800

    双指针

--- algorithm/src/leetcode/Q0018_4Sum.java
+++ algorithm/src/leetcode/Q0018_4Sum.java
@@ -42,6 +42,40 @@ public class Q0018_4Sum {
         return list;
     }
 
+    public List<List<Integer>> four(int[] nums, int target) {
+        List<List<Integer>> res = new LinkedList<>();
+        Arrays.sort(nums);
+        int n = nums.length;
+        for (int i = 0; i < n - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            if (nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3] > target) break;
+            if (nums[i] + nums[n - 1] + nums[n - 2] + nums[n - 3] < target) continue;
+            for (int j = i + 1; j < n - 2; j++) {
+                if (j - i > 1 && nums[j] == nums[j - 1]) continue;
+                if (nums[i] + nums[j] + nums[j + 1] + nums[j + 2] > target) break;
+                if (nums[i] + nums[j] + nums[n - 1] + nums[n - 2] < target) continue;
+
+                int left = j + 1;
+                int right = n - 1;
+                while (left < right) {
+                    int tmp = nums[i] + nums[j] + nums[left] + nums[right];
+                    if (tmp == target) {
+                        List<Integer> tmpList = new LinkedList<>(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+                        res.add(tmpList);
+                        while (left < right && nums[left] == nums[left + 1]) left += 1;
+                        while (left < right && nums[right] == nums[right - 1]) right -= 1;
+                        left += 1;
+                        right -= 1;
+                    } else if (tmp > target) right -= 1;
+                    else left += 1;
+                }
+            }
+
+        }
+
+        return res;
+    }
+
     public static void main(String[] args) {
        int[] a = {-3,-2,-1,0,0,1,2,3};
         fourSum(a, 0);
