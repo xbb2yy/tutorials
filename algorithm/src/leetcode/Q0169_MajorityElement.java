99978d62d6131f7d78b4576f1ddd798745f7afaa
xbb <xbbjava@163.com>
2019-10-14 11:50:22 +0800

    众数

---
+++ algorithm/src/leetcode/Q0169_MajorityElement.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Q0169_MajorityElement {
+
+    public static int majorityElement(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int thresh = nums.length / 2;
+        for (int i = 0; i < nums.length; i++) {
+            map.compute(nums[i], (key, old) -> old == null ? 1 : old + 1);
+        }
+        return map.entrySet().stream().filter(e -> e.getValue() > thresh).findFirst().get().getKey();
+    }
+
+    public static void main(String[] args) {
+        int[] a = {3, 2, 3};
+        majorityElement(a);
+    }
+}
