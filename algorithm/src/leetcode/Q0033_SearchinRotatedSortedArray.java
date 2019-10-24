38623fa1409429a73659bc7c3cd4a7fcd0dfc446
xbb <xbbjava@163.com>
2019-10-24 10:12:16 +0800

    q33

--- algorithm/src/leetcode/Q0033_SearchinRotatedSortedArray.java
+++ algorithm/src/leetcode/Q0033_SearchinRotatedSortedArray.java
@@ -1,8 +1,17 @@
 package leetcode;
 
+import sun.font.FontRunIterator;
+
 public class Q0033_SearchinRotatedSortedArray {
 
     public int search(int[] nums, int target) {
         return 0;
+    }
+
+    public int solution(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) return i;
+        }
+        return -1;
     }
 }
