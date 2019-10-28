77cc3c49d5f11549c05055b3cd94fef3ec7153ca
xbb <xbbjava@163.com>
2019-10-28 11:18:34 +0800

    矩阵置零

--- algorithm/src/leetcode/Q0073_SetMatrixZeroes.java
+++ algorithm/src/leetcode/Q0073_SetMatrixZeroes.java
@@ -1,8 +1,35 @@
 package leetcode;
 
+import leetcode.common.Pass;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Pass
 public class Q0073_SetMatrixZeroes {
 
     public void setZeroes(int[][] matrix) {
+        List<Integer> rows = new ArrayList<>();
+        List<Integer> cols = new ArrayList<>();
 
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][i] == 0) {
+                    rows.add(i);
+                    cols.add(j);
+                }
+            }
+        }
+
+        for (int i = 0; i < matrix.length; i++) {
+            if (rows.contains(i)) {
+                for (int j = 0; j < matrix[i].length; j++) {
+                    matrix[i][j] = 0;
+                }
+            }
+            for (Integer col : cols) {
+                matrix[i][col] = 0;
+            }
+        }
     }
 }
