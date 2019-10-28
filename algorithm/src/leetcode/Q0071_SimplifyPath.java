f1e5a70e47aa811492e1830dbeea3e7311347531
xbb <xbbjava@163.com>
2019-10-28 10:22:56 +0800

    简化路径

--- algorithm/src/leetcode/Q0071_SimplifyPath.java
+++ algorithm/src/leetcode/Q0071_SimplifyPath.java
@@ -1,9 +1,33 @@
 package leetcode;
 
+import java.util.LinkedList;
+
 public class Q0071_SimplifyPath {
 
-    public String simplifyPath(String path) {
+    public static String simplifyPath(String path) {
         String[] s = path.split("/");
-        return null;
+        LinkedList<String> stack = new LinkedList<>();
+        for (int i = 0; i < s.length; i++) {
+            if (s[i].equals("..")) {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                }
+            } else {
+                if (!s[i] .equals(".") && !s[i].isEmpty()) {
+                    stack.push(s[i]);
+                }
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()) {
+            sb.append("/").append(stack.removeLast());
+        }
+
+        return sb.toString().isEmpty() ? "/" : sb.toString();
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(simplifyPath("/home//foo"));
     }
 }
