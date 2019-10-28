0c1c907e7537d90c1cbd5b8844b2bba884b346de
xbb <xbbjava@163.com>
2019-10-28 09:45:40 +0800

    simplifypath

--- algorithm/src/leetcode/Q0054_SpiralMatrix.java
+++ algorithm/src/leetcode/Q0054_SpiralMatrix.java
@@ -1,9 +1,25 @@
 package leetcode;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Q0054_SpiralMatrix {
     public List<Integer> spiralOrder(int[][] matrix) {
-        return null;
+
+        List<Integer> list = new ArrayList<>();
+        int l =0, r = matrix[0].length, u = 0, b = matrix.length;
+        int flag = 1;
+        while (true) {
+            if (flag == 1) {
+                for (int i = 0; i < matrix[l].length; i++) {
+                    list.add(matrix[l][i]);
+                    u++;
+                    flag = 2;
+                }
+            }
+            if (flag == 2) {
+
+            }
+        }
     }
 }
---
+++ algorithm/src/leetcode/Q0071_SimplifyPath.java
@@ -0,0 +1,9 @@
+package leetcode;
+
+public class Q0071_SimplifyPath {
+
+    public String simplifyPath(String path) {
+        String[] s = path.split("/");
+        return null;
+    }
+}
