de3a82240ee55de03b842feb27d08f98616d2bbe
xbb <xbbjava@163.com>
2019-10-30 10:04:45 +0800

    翻转数组

--- algorithm/src/leetcode/Q0287_FindtheDuplicateNumber.java
+++ algorithm/src/leetcode/Q0287_FindtheDuplicateNumber.java
@@ -3,13 +3,11 @@ package leetcode;
 public class Q0287_FindtheDuplicateNumber {
 
     public int findDuplicate(int[] nums) {
-        int n = nums.length;
-        int exp = (n - 1) * n / 2;
-
-        int sum = 0;
+       int n = 0;
         for (int i = 0; i < nums.length; i++) {
-            sum += nums[i];
+            n^=nums[i];
+            if (n == 0) return nums[i];
         }
-        return sum - exp;
+        return -1;
     }
 }
