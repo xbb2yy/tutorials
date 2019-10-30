6f5e35c38ea5af40b1cbfbff5d2d18a75b095529
xbb <xbbjava@163.com>
2019-10-30 13:54:47 +0800

    旋转图像

--- algorithm/src/leetcode/Q0048_RotateImage.java
+++ algorithm/src/leetcode/Q0048_RotateImage.java
@@ -1,13 +1,28 @@
 package leetcode;
 
+import leetcode.common.Pass;
+
 import java.util.Arrays;
 
+@Pass
 public class Q0048_RotateImage {
 
     public void rotate(int[][] matrix) {
         int n = matrix.length;
         for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = tmp;
+            }
+        }
 
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - 1 - j];
+                matrix[i][n -1 - j] = tmp;
+            }
         }
     }
 
