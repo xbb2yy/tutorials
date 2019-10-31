7ae299359c9e5743c021d1e34826f216115bc756
xbb <xbbjava@163.com>
2019-10-31 13:49:30 +0800

    子集

--- algorithm/src/leetcode/Q0078_Subsets.java
+++ algorithm/src/leetcode/Q0078_Subsets.java
@@ -1,28 +1,38 @@
 package leetcode;
 
+import leetcode.common.Pass;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Pass
 public class Q0078_Subsets {
 
     public static List<List<Integer>> subsets(int[] nums) {
 
         List<List<Integer>> r =  new ArrayList<>();
-
-        if (nums == null )
+        if (nums == null ) {
             return null;
+        }
         if (nums.length == 0){
             return r;
         }
-        add(r, new ArrayList<>(), 0, nums);
+        r.add(new ArrayList<>());
+        add(r, 0, nums);
         return r;
     }
 
-    public static void add(List<List<Integer>> r, List<Integer> t, int i, int[] a) {
-        r.add(new ArrayList<>(t));
-        for (int j = i; j < a.length ; j++) {
+    public static void add(List<List<Integer>> r,  int i, int[] a) {
+        if (i > a.length - 1) return;
 
+        List<List<Integer>> tmp =  new ArrayList<>();
+        for (List<Integer> integers : r) {
+            ArrayList<Integer> list = new ArrayList<>(integers);
+            integers.add(a[i]);
+            tmp.add(list);
         }
+        r.addAll(tmp);
+        add(r, i + 1 , a);
     }
 
     public static void main(String[] args) {
