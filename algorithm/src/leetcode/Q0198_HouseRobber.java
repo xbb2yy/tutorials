634ddc8b440dc7b6eb93c074c7c87c33d114c265
xbb <xbbjava@163.com>
2019-11-04 15:58:03 +0800

    大打家劫舍

--- algorithm/src/leetcode/Q0198_HouseRobber.java
+++ algorithm/src/leetcode/Q0198_HouseRobber.java
@@ -2,7 +2,25 @@ package leetcode;
 
 public class Q0198_HouseRobber {
 
-    public int rob(int[] nums) {
-        return 0;
+    public static int rob(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        return help(nums, nums.length - 1);
+    }
+
+    private static int help(int[] nums, int n) {
+        if (n == 0) {
+            return nums[0];
+        }
+        if (n == 1) {
+            return Math.max(nums[0], nums[1]);
+        }
+        return Math.max(help(nums, n - 1), help(nums,n - 2) + nums[n]);
+    }
+
+    public static void main(String[] args) {
+        int[] a = {2, 7, 9, 3, 1};
+        System.out.println(rob(a));
     }
 }
