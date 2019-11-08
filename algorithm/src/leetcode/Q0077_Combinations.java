89a7f30eef3b4a744388ac197fca21b914d3c1c0
xbb <xbbjava@163.com>
2019-11-08 10:46:22 +0800

    组合

--- algorithm/src/leetcode/Q0077_Combinations.java
+++ algorithm/src/leetcode/Q0077_Combinations.java
@@ -1,4 +1,27 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Q0077_Combinations {
+
+    private List<List<Integer>> ans = new ArrayList<>();
+    public List<List<Integer>> combine(int n, int k) {
+        List<Integer> integers = new ArrayList<>(); ;
+        help(n, k,integers);
+        return ans;
+    }
+
+    private void help(int n, int k, List<Integer> list) {
+        if (k == 0) {
+            ans.add(list);
+            return;
+        }
+        for (int i = n; i >= n - k; i--) {
+            List<Integer> l = new ArrayList<>();
+            help(n - 1, k - 1, l);
+        }
+
+    }
+
 }
