0d01005d329ccc6660076cc049d9ed0cb5d25cdb
xbb <xbbjava@163.com>
2019-11-21 10:22:58 +0800

    二叉树中序遍历非递归

--- algorithm/src/leetcode/Q0094_BinaryTreeInorderTraversal.java
+++ algorithm/src/leetcode/Q0094_BinaryTreeInorderTraversal.java
@@ -5,6 +5,7 @@ import leetcode.common.TreeNode;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 @Pass
 public class Q0094_BinaryTreeInorderTraversal {
@@ -20,4 +21,21 @@ public class Q0094_BinaryTreeInorderTraversal {
         inorderTraversal(root.right);
         return list;
     }
+
+    public List<Integer> inorderTraversalUR(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack();
+        TreeNode cur = root;
+        while(cur != null || !stack.isEmpty()) {
+            while(cur != null) {
+                stack.push(cur);
+                cur = cur.left;
+            }
+            cur = stack.pop();
+            list.add(cur.val);
+            cur = cur.right;
+        }
+        return list;
+    }
+
 }
