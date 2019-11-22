29f0069cad10455836d3009dd85e9d9af15b1c36
xbb <xbbjava@163.com>
2019-11-22 15:42:24 +0800

    二叉树ç中的最大路径和

--- algorithm/src/leetcode/Q0124_BinaryTreeMaximumPathSum.java
+++ algorithm/src/leetcode/Q0124_BinaryTreeMaximumPathSum.java
@@ -3,8 +3,19 @@ package leetcode;
 import leetcode.common.TreeNode;
 
 public class Q0124_BinaryTreeMaximumPathSum {
+    int max = Integer.MIN_VALUE;
 
     public int maxPathSum(TreeNode root) {
-        int max = 0;
+        max(root);
+        return max;
+    }
+
+    private int max(TreeNode node) {
+        if (node == null) return 0;
+        int left = Math.max(max(node.left), 0);
+        int right = Math.max(max(node.right), 0);
+        int cur = left + right + node.val;
+        max = Math.max(max, cur);
+        return node.val + Math.max(left, right);
     }
 }
