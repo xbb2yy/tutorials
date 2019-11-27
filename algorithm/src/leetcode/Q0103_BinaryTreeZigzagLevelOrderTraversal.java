1d3773ac06c6787f9414ccbcd281869391d4bff4
xbb <xbbjava@163.com>
2019-11-27 13:50:20 +0800

    二叉树锯齿形遍历

--- algorithm/src/leetcode/Q0103_BinaryTreeZigzagLevelOrderTraversal.java
+++ algorithm/src/leetcode/Q0103_BinaryTreeZigzagLevelOrderTraversal.java
@@ -1,4 +1,26 @@
 package leetcode;
 
+import leetcode.common.TreeNode;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
 public class Q0103_BinaryTreeZigzagLevelOrderTraversal {
+
+
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if (root == null) return ans;
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        queue.add(root);
+        boolean fromLeft = false;
+
+
+        while (queue.peek() != null) {
+            TreeNode head = queue.poll();
+        }
+        return ans;
+    }
 }
