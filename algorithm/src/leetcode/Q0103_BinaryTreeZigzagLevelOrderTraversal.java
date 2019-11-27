965767cac38baf1c94e6821f3534839336eeb3b9
xbb <xbbjava@163.com>
2019-11-27 14:12:04 +0800

    二叉树的锯齿形层次遍历

--- algorithm/src/leetcode/Q0103_BinaryTreeZigzagLevelOrderTraversal.java
+++ algorithm/src/leetcode/Q0103_BinaryTreeZigzagLevelOrderTraversal.java
@@ -1,25 +1,38 @@
 package leetcode;
 
+import leetcode.common.Pass;
 import leetcode.common.TreeNode;
 
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Queue;
-
+import java.util.*;
+@Pass
 public class Q0103_BinaryTreeZigzagLevelOrderTraversal {
-
 
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         List<List<Integer>> ans = new ArrayList<>();
-        if (root == null) return ans;
+        if (root == null)
+            return ans;
         Queue<TreeNode> queue = new ArrayDeque<>();
         queue.add(root);
-        boolean fromLeft = false;
-
-
+        boolean fromLeft = true;
         while (queue.peek() != null) {
-            TreeNode head = queue.poll();
+            LinkedList<Integer> level = new LinkedList<>();
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode poll = queue.poll();
+                if (fromLeft) {
+                    level.addLast(poll.val);
+                } else {
+                    level.addFirst(poll.val);
+                }
+                if (poll.left != null) {
+                    queue.add(poll.left);
+                }
+                if (poll.right != null) {
+                    queue.add(poll.right);
+                }
+            }
+            ans.add(level);
+            fromLeft = !fromLeft;
         }
         return ans;
     }
