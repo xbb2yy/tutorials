a8785659d36497da2127c4323f63f32381e0e044
xbb <xbbjava@163.com>
2019-11-27 14:25:42 +0800

    二叉树展开为链表

--- algorithm/src/leetcode/Q0114_FlattenBinaryTreetoLinkedList.java
+++ algorithm/src/leetcode/Q0114_FlattenBinaryTreetoLinkedList.java
@@ -2,12 +2,29 @@ package leetcode;
 
 import leetcode.common.TreeNode;
 
+import java.util.Stack;
+
 public class Q0114_FlattenBinaryTreetoLinkedList {
 
     public void flatten(TreeNode root) {
         if (root == null) {
             return;
         }
-
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode tmp = null;
+        stack.push(root);
+        while (!stack.empty()) {
+            TreeNode top = stack.pop();
+            if (top.right != null) stack.push(top.right);
+            if (top.left != null) stack.push(top.left);
+            if (tmp == null) {
+                tmp = top;
+                tmp.left = null;
+            } else {
+                tmp.right = top;
+                tmp.left = null;
+                tmp = top;
+            }
+        }
     }
 }
