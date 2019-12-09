5ce1952379854150bb37edac61a001aa781f9a4a
xbb <xbbjava@163.com>
2019-12-09 10:31:45 +0800

    归并排序

--- algorithm/src/algorithm/c2/Merge.java
+++ algorithm/src/algorithm/c2/Merge.java
@@ -1,51 +1,25 @@
 package algorithm.c2;
 
 
-import static algorithm.c2.Example.*;
-
 public class Merge {
 
-    private static Comparable[] aux;
-
-    public static void sort(Comparable[] a) {
-        aux = new Comparable[a.length];
-        sort(a, 0, a.length - 1);
+    public static void sort(int[] nums) {
+        sort(nums, 0, nums.length - 1);
     }
 
-    private static void sort(Comparable[] a, int lo, int hi) {
-        if (hi <= lo)
-            return;
-        int mid = lo + (hi - lo) / 2;
-        sort(a, lo, mid);
-        sort(a, mid + 1, hi);
-        merge(a, lo, mid, hi);
+    private static void sort(int[] nums, int start, int end) {
+        if (start >= end) return;
+        int mid = start + (end - start) / 2;
+        sort(nums, start, mid);
+        sort(nums, mid + 1, end); 
+        merge(nums, start, end);
     }
 
-    public static void merge(Comparable[] a, int lo, int mid, int hi) {
-        int i = lo;
-        int j = mid + 1;
-        for (int k = lo; k <= hi; k++) {
-            aux[k] = a[k];
-        }
-
-        for (int k = lo; k <= hi; k++) {
-            if (i > mid)
-                a[k] = aux[j++];
-            else if (j > hi)
-                a[k] = aux[i++];
-            else if (less(aux[j], aux[i]))
-                a[k] = aux[j++];
-            else
-                a[k] = aux[i++];
-        }
+    private static void merge(int[] nums, int start, int end) {
     }
 
     public static void main(String[] args) {
-        Integer[] a = {32, 78, 22}; // , 34, 43, 2, 66, 23, 77, 55
+        int[] a = {32, 78, 22}; // , 34, 43, 2, 66, 23, 77, 55
         sort(a);
-        assert isSorted(a);
-        show(a);
     }
-
-
 }
