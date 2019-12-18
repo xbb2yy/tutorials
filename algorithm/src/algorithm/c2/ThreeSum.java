337232ad85d067d9cac58c1d63f09d41d9079bdd
xbb <xbbjava@163.com>
2019-12-18 16:20:16 +0800

    ThreeSum

---
+++ algorithm/src/algorithm/c2/ThreeSum.java
@@ -0,0 +1,17 @@
+package algorithm.c2;
+
+public class ThreeSum {
+
+    public static int count(int[] a) {
+        int N = a.length;
+        int cnt = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = i + 1; j < N; j++) {
+                for (int k = j + 1; k < N; k++) {
+                    if (a[i] + a[j] + a[k] == 0) cnt++;
+                }
+            }
+        }
+        return cnt;
+    }
+}
