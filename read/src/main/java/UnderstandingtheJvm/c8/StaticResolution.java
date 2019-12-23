3eccd6a4de29cb781f99b2594f04df88bd695cd3
xbb <xbbjava@163.com>
2019-12-23 14:39:40 +0800

    jvm

---
+++ read/src/main/java/UnderstandingtheJvm/c8/StaticResolution.java
@@ -0,0 +1,12 @@
+package UnderstandingtheJvm.c8;
+
+public class StaticResolution {
+
+    public static void sayHello() {
+        System.out.println("Hello World");
+    }
+
+    public static void main(String[] args) {
+        sayHello();
+    }
+}
