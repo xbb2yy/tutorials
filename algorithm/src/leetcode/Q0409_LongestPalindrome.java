b0af1cb15a5f27d2e3b2e49c613c760d95bd3947
xbb <xbbjava@163.com>
2019-12-24 16:49:54 +0800

    409

--- algorithm/src/leetcode/Q0409_LongestPalindrome.java
+++ algorithm/src/leetcode/Q0409_LongestPalindrome.java
@@ -16,4 +16,21 @@ public class Q0409_LongestPalindrome {
         if (count == 0) return s.length();
         return (int) (s.length() - count + 1);
     }
+
+    public int solution(String s) {
+        boolean[] a = new boolean[54];
+        for (int i = 0; i < s.length(); i++) {
+            a[s.charAt(i) - 65] = !a[s.charAt(i) - 65];
+        }
+        int count = 0;
+        for (int i = 0; i < a.length; i++) {
+            if (a[i]) count++;
+        }
+        if (count == 0) return s.length();
+        return (int) (s.length() - count + 1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println('a' - 65);
+    }
 }
