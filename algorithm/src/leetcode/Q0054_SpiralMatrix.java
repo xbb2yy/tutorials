447c78dd900cbef54edd1926d9ffc0303c4de186
xbb <xbbjava@163.com>
2019-12-26 16:06:19 +0800

    螺旋矩阵

--- algorithm/src/leetcode/Q0054_SpiralMatrix.java
+++ algorithm/src/leetcode/Q0054_SpiralMatrix.java
@@ -5,21 +5,27 @@ import java.util.List;
 
 public class Q0054_SpiralMatrix {
     public List<Integer> spiralOrder(int[][] matrix) {
-
-        List<Integer> list = new ArrayList<>();
-        int l =0, r = matrix[0].length, u = 0, b = matrix.length;
-        int flag = 1;
-        while (true) {
-            if (flag == 1) {
-                for (int i = 0; i < matrix[l].length; i++) {
-                    list.add(matrix[l][i]);
-                    u++;
-                    flag = 2;
-                }
-            }
-            if (flag == 2) {
-
+        List ans = new ArrayList();
+        if (matrix.length == 0) return ans;
+        int R = matrix.length, C = matrix[0].length;
+        boolean[][] seen = new boolean[R][C];
+        int[] dr = {0, 1, 0, -1};
+        int[] dc = {1, 0, -1, 0};
+        int r = 0, c = 0, di = 0;
+        for (int i = 0; i < R * C; i++) {
+            ans.add(matrix[r][c]);
+            seen[r][c] = true;
+            int cr = r + dr[di];
+            int cc = c + dc[di];
+            if (0 <= cr && cr < R && 0 <= cc && cc < C && !seen[cr][cc]){
+                r = cr;
+                c = cc;
+            } else {
+                di = (di + 1) % 4;
+                r += dr[di];
+                c += dc[di];
             }
         }
+        return ans;
     }
 }
