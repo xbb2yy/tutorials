51deb26b99aa25adb71413fb634a334208eb5984
xbb <xbbjava@163.com>
2019-12-26 16:23:51 +0800

    螺旋矩阵

--- algorithm/src/leetcode/Q0054_SpiralMatrix.java
+++ algorithm/src/leetcode/Q0054_SpiralMatrix.java
@@ -1,29 +1,35 @@
 package leetcode;
 
+import leetcode.common.Pass;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Pass
 public class Q0054_SpiralMatrix {
     public List<Integer> spiralOrder(int[][] matrix) {
-        List ans = new ArrayList();
-        if (matrix.length == 0) return ans;
-        int R = matrix.length, C = matrix[0].length;
-        boolean[][] seen = new boolean[R][C];
-        int[] dr = {0, 1, 0, -1};
-        int[] dc = {1, 0, -1, 0};
-        int r = 0, c = 0, di = 0;
-        for (int i = 0; i < R * C; i++) {
+       List<Integer> ans = new ArrayList<>();
+       if (matrix.length == 0) return ans;
+       int row = matrix.length;
+       int col = matrix[0].length;
+
+       int[] rDirection = {0, 1, 0, -1};
+       int[] cDirection = {1, 0, -1, 0};
+       int direction = 0, r = 0, c = 0;
+       boolean[][] seen = new boolean[row][col];
+
+        for (int i = 0; i < row * col; i++) {
             ans.add(matrix[r][c]);
             seen[r][c] = true;
-            int cr = r + dr[di];
-            int cc = c + dc[di];
-            if (0 <= cr && cr < R && 0 <= cc && cc < C && !seen[cr][cc]){
-                r = cr;
-                c = cc;
+            int rNext = r + rDirection[direction];
+            int cNext = c + cDirection[direction];
+            if (rNext >= 0 && rNext < row && cNext >= 0 && cNext < col && !seen[rNext][cNext]) {
+                r = rNext;
+                c = cNext;
             } else {
-                di = (di + 1) % 4;
-                r += dr[di];
-                c += dc[di];
+                direction = (direction + 1) % 4;
+                r = r + rDirection[direction];
+                c = c + cDirection[direction];
             }
         }
         return ans;
