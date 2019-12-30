b8f39c1419da1ebf7b13fc4a2e3637de9121037b
xbb <xbbjava@163.com>
2019-12-30 11:31:58 +0800

    解码方法

--- algorithm/src/leetcode/Q0091_DecodeWays.java
+++ algorithm/src/leetcode/Q0091_DecodeWays.java
@@ -4,19 +4,31 @@ public class Q0091_DecodeWays {
 
     public static int numDecodings(String s) {
 
-        int[] dp = new int[s.length()];
-        dp[0] = 1;
+       int[] dp = new int[s.length()];
+        if (s.charAt(0) == '0') {
+            return 0;
+        } else {
+            dp[0] = 1;
+        }
         for (int i = 1; i < s.length(); i++) {
-            if ((s.charAt(i - 1) - 48) * 10 + s.charAt(i) - 48 > 26) {
-                dp[i] = dp[i - 1];
-            } else {
+            if (s.charAt(i) == '0') {
+                if (s.charAt(i - 1) == '1' || s.charAt(i - 1) == '2') {
+                    dp[i] = dp[i - 1];
+                } else {
+                    return 0;
+                }
+                continue;
+            }
+            if ((s.charAt(i - 1) - 48) * 10 + s.charAt(i) - 48 <= 26 && (s.charAt(i - 1) - 48) * 10 + s.charAt(i) - 48 > 10) {
                 dp[i] = dp[i - 1] + 1;
+            } else {
+                dp[i] = dp[i - 1] ;
             }
         }
         return dp[s.length() - 1];
     }
 
     public static void main(String[] args) {
-        numDecodings("12");
+        System.out.println(numDecodings("10"));
     }
 }
