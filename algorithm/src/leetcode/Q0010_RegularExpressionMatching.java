739b7f440cf7eaf6079ce7cb8ac0721c8b2c38e2
xbb <xbbjava@163.com>
2019-12-31 11:08:21 +0800

    正则表达式匹配

---
+++ algorithm/src/leetcode/Q0010_RegularExpressionMatching.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+public class Q0010_RegularExpressionMatching {
+
+    public boolean isMatch(String s, String p) {
+        if (p.isEmpty()) return s.isEmpty();
+        boolean match = !s.isEmpty() && s.charAt(0) == p.charAt(0) || p.charAt(0) == '.';
+
+        if (p.length() >= 2 && p.charAt(1) == '.') {
+            return (isMatch(s, p.substring(2)) ||
+                    (match && isMatch(s.substring(1), p)));
+
+        } else {
+            return match && isMatch(s.substring(1), p.substring(1));
+        }
+    }
+}
