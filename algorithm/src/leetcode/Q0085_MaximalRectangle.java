95d0dcb5fd62f92429942ec50c1b3edbd81e9b3b
xbb <xbbjava@163.com>
2020-01-08 16:24:50 +0800

    最大矩形unresolved

---
+++ algorithm/src/leetcode/Q0085_MaximalRectangle.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+public class Q0085_MaximalRectangle {
+
+    public int maximalRectangle(char[][] matrix) {
+
+        int max = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+
+            }
+        }
+        return max;
+    }
+}
