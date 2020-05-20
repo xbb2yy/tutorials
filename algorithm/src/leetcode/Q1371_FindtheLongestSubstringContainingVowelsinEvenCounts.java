f2c46e24a72998dd2e47f92518d54d987e5af3ec
xbb <xbbjava@163.com>
2020-05-20 10:13:24 +0800

    1371

---
+++ algorithm/src/leetcode/Q1371_FindtheLongestSubstringContainingVowelsinEvenCounts.java
@@ -0,0 +1,9 @@
+package leetcode;
+
+public class Q1371_FindtheLongestSubstringContainingVowelsinEvenCounts {
+
+    public int findTheLongestSubstring(String s) {
+        int max = 0;
+        return max;
+    }
+}
